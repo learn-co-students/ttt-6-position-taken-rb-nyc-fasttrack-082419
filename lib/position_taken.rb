231879730cbba62413d5ab978575a5417f21f506
Emirhan Kaplan <emskaplann@gmail.com>
2019-08-24 19:54:50 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,7 +2,6 @@
 def position_taken?(board, index)
   if board[index] == " " || board[index] == ""
     return false
-
   elsif board[index] == "X" || board[index] == "O"
   return true
   elsif board[index] == nil
